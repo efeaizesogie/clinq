fba47c22270da897c073f9b7f1f10dccd0e7cdca
efeaizesogie <efeaizesogie@gmail.com>
2026-07-30 12:12:37 +0100

    feat: implement patient dashboard layout with persistent theme synchronization

--- src/app/patient/layout.tsx
+++ src/app/patient/layout.tsx
@@ -79,7 +79,7 @@ export default function PatientLayout({
       <PatientSidebar isOpen={sidebarOpen} onClose={() => setSidebarOpen(false)} />
 
       {/* Main Content Area */}
-      <div className="flex-1 flex flex-col min-h-screen md:ml-[256px] relative">
+      <div className="flex-1 min-w-0 flex flex-col min-h-screen md:ml-[256px] relative">
         {/* Top Header navbar */}
         <PatientTopNav onMenuToggle={() => setSidebarOpen(true)} />
 
